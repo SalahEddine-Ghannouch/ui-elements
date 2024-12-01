1ca27b4fc7422ee828f0bc0f3fc03430f848b515
Salah Eddine <79339578+SalahEddine-Ghannouch@users.noreply.github.com>
2024-12-01 14:30:37 +0100

    Create ui-loading.jsx

---
+++ ui-loading.jsx
@@ -0,0 +1,9 @@
+/* Loading ui element */ 
+<div class="flex-col space-y-4 items-center justify-center">
+<div class="flex items-center justify-center border-8 border-gray-400 rounded-full w-28 h-28 border-blue-400 border-t-blue-500 animate-bounce ">
+....
+</div>
+<div class="flex items-center justify-center border-8 border-gray-500 rounded-full w-28 h-28 border-blue-400 border-t-blue-500 text-white animate-spin">
+....
+</div>
+</div>
